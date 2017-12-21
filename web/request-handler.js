ccd057d12f0ab740fbcf9307f8559aff808d9104
decross1 <decross1@me.com>
2017-12-20 16:20:20 -0800

    'Serving up static files'

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,5 +1,7 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+
 // require more modules/folders here!
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
@@ -8,15 +10,40 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
+
 exports.handleRequest = function (req, res) {
+  // console.log('inside handleRequest');
+
   var headers = defaultCorsHeaders;
-  if ( req.method === 'GET' && req.url === paths.staticFiles) {
-    headers['Content-Type'] = 'text/html';
-  } else {
+  if ( req.method === 'GET' && req.url === '/') {  
+    
+    fs.readFile(archive.paths.staticFiles + '/index.html', (err, data) => {
+      if (err) { throw err; }
+      headers['Content-Type'] = 'text/html';
+      res.write(data.toString());
+      res.end();
+    });
+    
+  } else if (req.method === 'GET' && req.url === '/styles.css') {
+    
+    fs.readFile(archive.paths.staticFiles + '/styles.css', (err, data) => {
+      if (err) { throw err; }
+      headers['Content-Type'] = 'text/css';
+      res.write(data.toString());
+      res.end();
+    });
+  
+  } 
+  // else if (req.method === 'GET' && req.url === '/www.google.com') {
+  //   console.log('working');
+  //   // fs.readFile(archive.paths);
+  // } 
+  else {
+    console.log('requested url', req.url);
     res.writeHead(404, headers);
     res.end('Error');
   }
-  res.end(archive.paths.list); //sends url txt to sites.txt?
+  // res.end(archive.paths.list); //sends url txt to sites.txt?
 };
 
 
